0dfdf002440cb06a7587beb1e36f3b28251e91f5
Ovyerus <iamovyerus@gmail.com>
2021-02-27 17:07:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Twemoji
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `twemoji` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:twemoji, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with
+[ExDoc](https://github.com/elixir-lang/ex_doc) and published on
+[HexDocs](https://hexdocs.pm). Once published, the docs can be found at
+[https://hexdocs.pm/twemoji](https://hexdocs.pm/twemoji).
---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Twemoji.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :twemoji,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "Twemoji for Elixir!",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/ovyerus/twemoji-elixir"}
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:emojix, "~> 0.3.0"},
+      {:floki, "~> 0.30.0"}
+    ]
+  end
+end
